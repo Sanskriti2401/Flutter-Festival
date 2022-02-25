33da9fc615241848343ca21f38a369dfebb9f0c2
Sanskriti Negi <sanskritinegi2401@gmail.com>
2022-02-25 20:21:30 +0530

    Create question 2.dart

---
+++ Task1/question 2.dart
@@ -0,0 +1,20 @@
+bool isEven(int n)
+{
+    if(n%2==0)
+    {
+      return true;
+    }
+    return false;
+}
+void main() {
+  
+  if(isEven(9))
+  {
+     print("number is Even");
+  }
+  else
+  {
+       print("number is odd");
+  }
+  
+}
