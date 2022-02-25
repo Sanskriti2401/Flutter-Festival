a5e2c11c2acc2116bccd2a4f176c8059f8c4c6db
Sanskriti Negi <sanskritinegi2401@gmail.com>
2022-02-25 19:49:06 +0530

    Create question 4.dart

---
+++ Task1/question 4.dart
@@ -0,0 +1,24 @@
+void main() {
+  
+  
+  sum(48,2);
+  sub(67,7);
+  mul(45,7);
+  div(40,2);
+
+}
+
+void sum(int a,int b)=>(print("Addition= ${a+b}"));
+void sub(int a,int b)=>(print("Subtraction= ${a-b}"));
+void mul(int a,int b)=>(print("Multiplication= ${a*b}"));
+void div(int a,int b)=>(print("Division= ${a/b}"));
+
+
+/*
+Output:
+Addition= 50
+Subtraction= 60
+Multiplication= 315
+Division= 20
+
+*/
