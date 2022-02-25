5e3336078463213b00d343ac2bbf7490b8ae0767
Sanskriti Negi <sanskritinegi2401@gmail.com>
2022-02-25 20:19:53 +0530

    Create question 9.dart

---
+++ Task1/question 9.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  isAlpha('h');
+}
+
+
+void isAlpha(String a)
+{
+  int n=a.codeUnitAt(0);
+  n>=65 && n<=90 || n>=97 && n<=122 ? print("Character is Alphabet"):print("Character is not Alphabet");
+  
+  
+}
+  
+/*Output:
+Character is Alphabet
+
+
+*/
