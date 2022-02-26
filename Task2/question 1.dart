3f2830ac497354eafcbff3b48f18cefb491dd2c7
Sanskriti Negi <sanskritinegi2401@gmail.com>
2022-02-26 21:03:13 +0530

    Create question 1.dart

---
+++ Task2/question 1.dart
@@ -0,0 +1,9 @@
+MCQ QUESTIONS:
+
+i)    c : int multiply(int a, int b) => a * b; 
+ii)   2 : Async functions
+iii)  3: List
+iv)   3: Error generated
+v)    4: None
+vi)   2: nums.forEach(print);
+vii)  1,2,4
